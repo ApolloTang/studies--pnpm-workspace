0e587605f6e2315d0b09fc09fcb3069c991cfc85
Apollo Tang <tang.apollo@gmail.com>
2021-04-23 16:01:06 -0400

    fix bug

--- 03-filter-studies-with-public/workspace/pkgs-consumer/consumer-pkg-y/src/index.js
+++ 03-filter-studies-with-public/workspace/pkgs-consumer/consumer-pkg-y/src/index.js
@@ -1,9 +1,12 @@
 const pkgJson = require('../package.json')
 
-const dependency = require('@apollotang/local-pkg-c')
+const dependency1 = require('@apollotang/public-pkg-c')
+let output1 = `${pkgJson.name}:${pkgJson.version} ---> ${dependency1.name}:${dependency1.version}`
+console.log(output1)
 
-let output = `${pkgJson.name}:${pkgJson.version} ---> ${dependency.name}:${dependency.version}`
-console.log(output)
+const dependency2 = require('@apollotang/local-pkg-d')
+let output2 = `${pkgJson.name}:${pkgJson.version} ---> ${dependency2.name}:${dependency2.version}`
+console.log(output2)
 
 module.exports = pkgJson
 
